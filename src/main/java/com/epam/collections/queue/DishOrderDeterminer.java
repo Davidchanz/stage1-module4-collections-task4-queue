48edb1d5770b693423998fea387b5f8ee4411344
Davidchanz <tovliskatsi@gmail.com>
2023-07-25 19:17:40 +0400

    Collections Task 4 Queue completed

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,26 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> result = new ArrayList<>();
+        LinkedList<Integer> dishes = new LinkedList<>();
+        for(int i = 1; i <= numberOfDishes; i++){
+            dishes.add(i);
+        }
+        int dishNumber = 1;
+        int currentDish = 0;
+        while (!dishes.isEmpty()){
+            if(dishNumber++ == everyDishNumberToEat){
+                result.add(dishes.get(currentDish));
+                dishes.remove(currentDish--);
+                dishNumber = 1;
+            }
+            currentDish++;
+            if(currentDish >= dishes.size())
+                currentDish = 0;
+        }
+        return result;
     }
 }
